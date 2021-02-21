695fae8ed69bf2e3c1971991eea0aaa2b5da2042
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-02-20 20:44:09 -0700

    added inquirer prompt questions

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Runs the application
+// index.js runs the application
+const fs = require ('fs');
+const inquirer = require ('inquirer');
+const Employee = require ('./lib/Employee');
+const Engineer = require ('./lib/Engineer');
+const Intern = require ('./lib/Intern');
+const Manager = require ('./lib/Manager');
+
+const employees = [];
+
+function createTeam() {
+  inquirer.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Enter team member name.',
+  },
+  {
+   type: 'list',
+   name: 'role',
+   message: "Select team member's role.",
+   choices: ['Manager', 'Engineer', 'Intern'],
+  },
+  {
+    type: 'input',
+    name: 'ID',
+    message: 'Enter team member ID number.',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Enter team member's email address.",
+  }])
+  
+}
