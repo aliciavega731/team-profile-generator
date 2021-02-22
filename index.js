b388b239d99dcf90d48d8f1de1b91245198fc101
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-02-21 18:15:40 -0700

    updated code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js runs the application
 const fs = require ('fs');
 const inquirer = require ('inquirer');
-const Employee = require ('./lib/Employee');
+// const Employee = require ('./lib/Employee');
 const Engineer = require ('./lib/Engineer');
 const Intern = require ('./lib/Intern');
 const Manager = require ('./lib/Manager');
@@ -19,13 +19,13 @@ function addManager() {
   },
   {
     type: "input",
-    name: "email",
-    message: "Enter manager's email address:",
+    name: "id",
+    message: "Enter manager's ID number:",
   },
   {
     type: "input",
-    name: "id",
-    message: "Enter manager's ID number:",
+    name: "email",
+    message: "Enter manager's email address:",
   },
   {
     type: "input",
@@ -50,13 +50,13 @@ function addEngineer() {
   },
   {
     type: "input",
-    name: "email",
-    message: "Enter engineer's email address:",
+    name: "id",
+    message: "Enter engineer's ID number:",
   },
   {
     type: "input",
-    name: "id",
-    message: "Enter engineer's ID number:",
+    name: "email",
+    message: "Enter engineer's email address:",
   },
   {
     type: "input",
@@ -81,13 +81,13 @@ function addIntern() {
   },
   {
     type: "input",
-    name: "email",
-    message: "Enter intern's email address:",
+    name: "id",
+    message: "Enter intern's ID number:",
   },
   {
     type: "input",
-    name: "id",
-    message: "Enter intern's ID number:",
+    name: "email",
+    message: "Enter intern's email address:",
   },
   {
     type: "input",
@@ -119,16 +119,67 @@ function addAnother(){
     } else if(responses.addMore === "Add intern"){
       addIntern()
     } else {
-      //generateHtml()
+      createHtml()
     }
   })
 }
 
-// function generateHtml(){
-//   console.log("Let's build the HTML")
-//   let myString = `
-//   `
-//   fs.writeFile("./lib/generateHTML", myString, function(e){console.error(e)})
-// }
+function createHtml(){
+  // console.log("Time to build the HTML")
+  let myString = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <title>Team Profile Generator</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-dark bg-primary">
+    <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+  </nav>
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <div class="card mx-auto mb-3" style="width: 15rem">
+          <h3 class="card-header">Manager name<br /><br />Manager</h3>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: Manager ID</li>
+            <li class="list-group-item">Email address: Manager id</li>
+            <li class="list-group-item">Office number: Manager office number</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="card mx-auto mb-3" style="width: 15rem">
+          <h5 class="card-header">Engineer name<br /><br />engineer</h5>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID:id here </li>
+            <li class="list-group-item">Email address: email here</li>
+            <li class="list-group-item">Github: git hub</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="card mx-auto mb-3" style="width: 15rem">
+          <h5 class="card-header">Intern name<br /><br />intern</h5>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: id here</li>
+            <li class="list-group-item">Email address: email here</li>
+            <li class="list-group-item">School: school here</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
+  `
+  fs.writeFile("./dist/createHtml.html", myString, function(e){console.error(e)})
+}
 
 addManager()
